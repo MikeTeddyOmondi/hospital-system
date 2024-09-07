5eb962bfa5090af738b565c8d0d919245cda83da
MikeTeddyOmondi <mike_omondi@outlook.com>
2024-09-07 13:12:22 +0300

    Updated maxAge field in express static options config

--- app/server.js
+++ app/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public'), { maxAge: '1h' }));
 
 // Session Store Options
 const sessionStoreOptions = {
